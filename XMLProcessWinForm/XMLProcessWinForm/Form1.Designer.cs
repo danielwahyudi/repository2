2843241ce94e7cb0c934540b4757a178c792fc5c
danielwahyudi <danielw74@gmail.com>
2017-07-14 16:09:01 +1000

    XML Process project
    XML Project using two threads to read XML files and generate HTML pages

---
+++ XMLProcessWinForm/XMLProcessWinForm/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace XMLProcessWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnSelectXMLSource = new System.Windows.Forms.Button();
+            this.lblXMLSource = new System.Windows.Forms.Label();
+            this.lblXMLNumFiles = new System.Windows.Forms.Label();
+            this.btnSelectOutputFolder = new System.Windows.Forms.Button();
+            this.lblOutputFolder = new System.Windows.Forms.Label();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSelectXMLSource
+            // 
+            this.btnSelectXMLSource.Location = new System.Drawing.Point(13, 13);
+            this.btnSelectXMLSource.Name = "btnSelectXMLSource";
+            this.btnSelectXMLSource.Size = new System.Drawing.Size(117, 23);
+            this.btnSelectXMLSource.TabIndex = 0;
+            this.btnSelectXMLSource.Text = "Select XML Folder";
+            this.btnSelectXMLSource.UseVisualStyleBackColor = true;
+            this.btnSelectXMLSource.Click += new System.EventHandler(this.btnSelectXMLSource_Click);
+            // 
+            // lblXMLSource
+            // 
+            this.lblXMLSource.AutoSize = true;
+            this.lblXMLSource.Location = new System.Drawing.Point(162, 18);
+            this.lblXMLSource.Name = "lblXMLSource";
+            this.lblXMLSource.Size = new System.Drawing.Size(67, 13);
+            this.lblXMLSource.TabIndex = 1;
+            this.lblXMLSource.Text = "Not selected";
+            // 
+            // lblXMLNumFiles
+            // 
+            this.lblXMLNumFiles.AutoSize = true;
+            this.lblXMLNumFiles.Location = new System.Drawing.Point(162, 42);
+            this.lblXMLNumFiles.Name = "lblXMLNumFiles";
+            this.lblXMLNumFiles.Size = new System.Drawing.Size(0, 13);
+            this.lblXMLNumFiles.TabIndex = 2;
+            // 
+            // btnSelectOutputFolder
+            // 
+            this.btnSelectOutputFolder.Enabled = false;
+            this.btnSelectOutputFolder.Location = new System.Drawing.Point(13, 57);
+            this.btnSelectOutputFolder.Name = "btnSelectOutputFolder";
+            this.btnSelectOutputFolder.Size = new System.Drawing.Size(117, 23);
+            this.btnSelectOutputFolder.TabIndex = 3;
+            this.btnSelectOutputFolder.Text = "Select Output Folder";
+            this.btnSelectOutputFolder.UseVisualStyleBackColor = true;
+            this.btnSelectOutputFolder.Click += new System.EventHandler(this.btnSelectOutputFolder_Click);
+            // 
+            // lblOutputFolder
+            // 
+            this.lblOutputFolder.AutoSize = true;
+            this.lblOutputFolder.Location = new System.Drawing.Point(162, 62);
+            this.lblOutputFolder.Name = "lblOutputFolder";
+            this.lblOutputFolder.Size = new System.Drawing.Size(67, 13);
+            this.lblOutputFolder.TabIndex = 4;
+            this.lblOutputFolder.Text = "Not selected";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Enabled = false;
+            this.btnSubmit.Location = new System.Drawing.Point(13, 102);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(117, 23);
+            this.btnSubmit.TabIndex = 5;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Location = new System.Drawing.Point(13, 131);
+            this.tbOutput.Multiline = true;
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.ReadOnly = true;
+            this.tbOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbOutput.Size = new System.Drawing.Size(447, 144);
+            this.tbOutput.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 291);
+            this.Controls.Add(this.tbOutput);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.lblOutputFolder);
+            this.Controls.Add(this.btnSelectOutputFolder);
+            this.Controls.Add(this.lblXMLNumFiles);
+            this.Controls.Add(this.lblXMLSource);
+            this.Controls.Add(this.btnSelectXMLSource);
+            this.Name = "Form1";
+            this.Text = "XML Process";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnSelectXMLSource;
+        private System.Windows.Forms.Label lblXMLSource;
+        private System.Windows.Forms.Label lblXMLNumFiles;
+        private System.Windows.Forms.Button btnSelectOutputFolder;
+        private System.Windows.Forms.Label lblOutputFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox tbOutput;
+    }
+}
+
---
+++ XMLProcessWinForm/XMLProcessWinForm/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Xsl;
+
+namespace XMLProcessWinForm
+{
+    /* Desktop application to read a collection of XML files and render them into HTML pages.
+     * Created by Daniel Wahyudi
+    */
+    public partial class Form1 : Form
+    {
+        // XSLT file location. Defined in App.config settings file so the location can be changed without recompiling the application.
+        string xsltFilePath = System.Configuration.ConfigurationManager.AppSettings.Get("xsltFilePath");
+
+        static Queue queue = Queue.Synchronized(new Queue());
+
+        private delegate void DisplayOutputDelegate(string message);
+        private delegate void EnableButtonDelegate();
+
+        private void DisplayOutput(string message)
+        {
+            tbOutput.Text += message;
+        }
+
+        private void EnableButton()
+        {
+            btnSubmit.Enabled = true;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Select a folder where the XML source files are located
+        /// </summary>
+        private void btnSelectXMLSource_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string[] files = Directory.GetFiles(folderBrowserDialog1.SelectedPath);                
+
+                lblXMLSource.Text = folderBrowserDialog1.SelectedPath;
+                lblXMLNumFiles.Text = files.Length.ToString() + " file(s) found.";
+
+                btnSelectOutputFolder.Enabled = true;
+            }
+        }
+
+        /// <summary>
+        /// Set the folder where the HTML files will be generated.
+        /// </summary>
+        private void btnSelectOutputFolder_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog2.ShowDialog();
+            lblOutputFolder.Text = folderBrowserDialog2.SelectedPath;
+            btnSubmit.Enabled = true;
+        }
+
+        /// <summary>
+        /// Reads all the XML files in the "source" folder and put the XML contents in the queue.
+        /// </summary>
+        private void ReadXMLFile()
+        {
+            try
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(lblXMLSource.Text);
+                FileInfo[] xmlFiles = directoryInfo.GetFiles("*.xml");
+                string tmpContents = string.Empty;
+
+                // Put the XML files in a queue
+                for (int i = 0; i < xmlFiles.Count(); i++)
+                {
+                    tbOutput.Invoke(new DisplayOutputDelegate(DisplayOutput), string.Format("Reading {0}...{1}", xmlFiles[i].Name, Environment.NewLine));                    
+
+                    using (FileStream fileStream = File.OpenRead(xmlFiles[i].FullName))
+                    {
+                        MemoryStream memoryStream = new MemoryStream();
+                        memoryStream.SetLength(fileStream.Length);
+                        // Read file contents to memory stream object
+                        fileStream.Read(memoryStream.GetBuffer(), 0, (int)fileStream.Length);
+                        queue.Enqueue(memoryStream);
+
+                        tbOutput.Invoke(new DisplayOutputDelegate(DisplayOutput), "Reading XML completed." + Environment.NewLine);
+
+                        Thread.Sleep(1000);
+                    }
+                }
+
+                // Enable "Submit" button after process is completed
+                btnSubmit.Invoke(new EnableButtonDelegate(EnableButton));
+
+                MessageBox.Show("Process completed.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Gets all the XML files contents from the queue and creates the HTML file.
+        /// </summary>
+        private void GenerateHTMLFile()
+        {            
+            try
+            {
+                int i = 0;  // For HTML filename
+
+                while (true)
+                {
+                    if (queue.Count > 0)
+                    {
+                        string htmlFullFileName = string.Format("{0}\\ComputerReport_{1}.html", lblOutputFolder.Text, i + 1);
+                        tbOutput.Invoke(new DisplayOutputDelegate(DisplayOutput), string.Format("Generating {0}...{1}", htmlFullFileName, Environment.NewLine));
+
+                        // Get the XSLT template
+                        XslCompiledTransform xslCompiledTransform = new XslCompiledTransform();
+                        xslCompiledTransform.Load(xsltFilePath);
+
+                        // Use StringBuilder object to hold HTML data and creates TextWriter object to hold data from xslCompiledTransform
+                        StringBuilder sbHtmlOutput = new StringBuilder();
+                        TextWriter textWriter = new StringWriter(sbHtmlOutput);
+
+                        // Create XmlReader object to read XML content                           
+                        MemoryStream memoryStream = (MemoryStream)queue.Dequeue();
+                        XmlReader reader = XmlReader.Create(memoryStream);
+
+                        // Create HTML string and write in TextWriter object.    
+                        xslCompiledTransform.Transform(reader, null, textWriter);
+
+                        // Create HTML page
+                        File.WriteAllText(htmlFullFileName, sbHtmlOutput.ToString());
+
+                        tbOutput.Invoke(new DisplayOutputDelegate(DisplayOutput), "HTML page created." + Environment.NewLine);
+
+                        Thread.Sleep(100);
+                        i++;
+                    }                                    
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            btnSubmit.Enabled = false;
+
+            try
+            {
+                // The first thread. Read sequentially through the XML files and put their contents on the queue.
+                Thread producer = new Thread(() =>
+                {
+                    ReadXMLFile();
+                });
+
+                // The second thread. Read from the queue and render the XML to HTML.
+                Thread consumer = new Thread(() =>
+                {
+                    GenerateHTMLFile();
+                });
+                consumer.IsBackground = true;
+
+                // Start the threads
+                consumer.Start();
+                producer.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
